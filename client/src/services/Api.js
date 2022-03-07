70404258720777566fa0adde14f72e1282338b96
Kaito <hoangdai.d93@gmail.com>
2022-03-07 19:39:50 +0100

    Removed comment

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 export default () => {
   return axios.create({
     baseURL: `${process.env.VUE_APP_API_BASE_URL}`,
-    // baseURL: `http://localhost:8000/api`,
     withCredentials: true
   })
 }
