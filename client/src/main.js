58a0dc1652c568e27686c45bdb87350eac074bd0
Kaito <hoangdai.d93@gmail.com>
2022-04-02 15:11:13 +0200

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -2,10 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import "@/assets/global.css";
-import "@/assets/theme_lily.css";
-import "@/assets/theme_iris.css";
-
 import store from './store/index'
 
 createApp(App).use(router).use(store).mount('#app')
