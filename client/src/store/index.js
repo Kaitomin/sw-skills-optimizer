e93f089c4bf6d53362463f908ad586b0ace2747f
Kaito <hoangdai.d93@gmail.com>
2022-04-05 22:47:13 +0200

    Update index.js

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,9 +5,11 @@ const store = createStore({
     chainsArray: JSON.parse(localStorage.getItem('chains')) ?? [],
     lilyChains: JSON.parse(localStorage.getItem('lily-chains')) ?? [],
     irisChains: JSON.parse(localStorage.getItem('iris-chains')) ?? [],
+    stellaChains: JSON.parse(localStorage.getItem('stella-chains')) ?? [],
     rotations: new Map(JSON.parse(localStorage.getItem('rotations'))),
     lilyRotations: new Map(JSON.parse(localStorage.getItem('lily-rotations'))),
-    irisRotations: new Map(JSON.parse(localStorage.getItem('iris-rotations')))
+    irisRotations: new Map(JSON.parse(localStorage.getItem('iris-rotations'))),
+    stellaRotations: new Map(JSON.parse(localStorage.getItem('stella-rotations')))
   },
   mutations: {
     saveChains(state, template) {
@@ -29,6 +31,15 @@ const store = createStore({
           localStorage.setItem('iris-chains', JSON.stringify(state.irisChains))
         }
       }
+      if (template.name == 'Stella') {
+        if (template.ids.length > 0) {
+          state.stellaChains = state.stellaChains.filter(c => !template.ids.includes(c.chains[0]))
+          localStorage.setItem('stella-chains', JSON.stringify(state.stellaChains))
+        } else {
+          state.stellaChains = template.chains
+          localStorage.setItem('stella-chains', JSON.stringify(state.stellaChains))
+        }
+      }
     },
     saveRotations(state, template) {
       if (template.name == 'Lily') {
@@ -39,6 +50,11 @@ const store = createStore({
       if (template.name == 'Iris') {
         state.irisRotations = template.rotations
         localStorage.setItem('iris-rotations', state.irisRotations);
+        return
+      }
+      if (template.name == 'Stella') {
+        state.stellaRotations = template.rotations
+        localStorage.setItem('stella-rotations', state.stellaRotations);
         return
       }
     },
@@ -56,11 +72,17 @@ const store = createStore({
     irisChains (state) {
       return state.irisChains;
     },
+    stellaChains (state) {
+      return state.stellaChains;
+    },
     lilyRotations (state) {
       return state.lilyRotations;
     },
     irisRotations (state) {
       return state.irisRotations;
+    },
+    stellaRotations (state) {
+      return state.stellaRotations;
     }
   }
 })
