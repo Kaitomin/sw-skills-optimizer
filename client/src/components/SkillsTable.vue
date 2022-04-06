60a086c6842612c4da34d5999782885c7f4567ad
Kaito <hoangdai.d93@gmail.com>
2022-04-06 16:36:44 +0200

    Changed DW coeff

--- client/src/components/SkillsTable.vue
+++ client/src/components/SkillsTable.vue
@@ -118,7 +118,7 @@ export default {
 
         Array.from(this.skillsTable).map(skill => {
           if (skill.dwBoost) {
-            skill.dmg = Math.round(skill.dmg * 1.6)
+            skill.dmg = Math.round(skill.dmg * 1.58)
           } else {
             skill.dmg = Math.round(skill.dmg * 1.2)
           }
