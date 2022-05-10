30c34089eab8af368c5481fd710e4eac780fff0a
Kaito <hoangdai.d93@gmail.com>
2022-05-10 17:23:56 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -35,9 +35,9 @@ app.use('/api', skillsRoutes);
 app.use('/api', userRoutes);
 
 // Serve file after refresh page
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
 
 // DB connection
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
