6dd2a0e5f7bfca8439327141b9fe10e5cfad7dd7
Kaito <hoangdai.d93@gmail.com>
2022-06-11 16:28:49 +0200

    Update CharactersList.vue

--- client/src/views/CharactersList.vue
+++ client/src/views/CharactersList.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="characters-list">
-    <!-- <div :class="char.name == 'Nabi' ? 'hidden' : ''" v-for="char in charList" :key="char._id"> -->
-    <div :class="(char.name == 'Dana' || char.name == 'Erwin') ? 'hidden' : ''" v-for="char in charList" :key="char._id">
-    <!-- <div :class="(char.name == 'Dana') ? 'hidden' : ''" v-for="char in charList" :key="char._id"> -->
+    <div :class="(char.name == 'Dana') ? 'hidden' : ''" v-for="char in charList" :key="char._id">
       <router-link :to="'/character/' + char.name">
         <img
           :src="getImgUrl(char.icon)"
