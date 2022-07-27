9b42b8985110a471143a8596069ca2c59915f97c
Kaito <hoangdai.d93@gmail.com>
2022-07-27 22:46:14 +0200

    Update App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,57 +1,18 @@
 <template>
   <div>
-    <div id="nav">
-      <router-link to="/" class="logo">
-        <img src="@/assets/img/logo_130x70.png" alt="soulworker logo" width="130" height="70">
-      </router-link>
-      <div class="menu">
-        <router-link to="/character/Iris">
-          <img src="@/assets/img/iris_nav.png" alt="iris nav icon" width="120" height="50">
-        </router-link>
-        <router-link to="/character/Lily">
-          <img src="@/assets/img/lily_nav.png" alt="lily nav icon" width="120" height="50">
-        </router-link>
-        <router-link to="/character/Haru">
-          <img src="@/assets/img/haru_nav.png" alt="haru nav icon" width="120" height="50">
-        </router-link>
-        <router-link to="/character/Stella">
-          <img src="@/assets/img/stella_nav.png" alt="stella nav icon" width="120" height="50">
-        </router-link>
-        <router-link to="/character/Ephnel">
-          <img src="@/assets/img/ephnel_nav.png" alt="ephnel nav icon" width="120" height="50">
-        </router-link>
-        <router-link to="/character/Chii">
-          <img src="@/assets/img/chii_nav.png" alt="chii nav icon" width="120" height="50">
-        </router-link>
-        <router-link to="/character/Nabi">
-          <img src="@/assets/img/nabi_nav.png" alt="nabi nav icon" width="120" height="50">
-        </router-link>
-        <router-link to="/character/Erwin">
-          <img src="@/assets/img/erwin_nav.png" alt="nabi nav icon" width="120" height="50">
-        </router-link>
-      </div>
-      <div>
-        <router-link to="/calculator"><i class="fa-solid fa-calculator"></i> Calculator</router-link>
-      </div>
-      <div v-if="userRole == 'ADMIN'" class="edit">
-        <router-link to="/add-new-char" >Add character</router-link><div class="separator"></div>
-        <router-link to="/add-new-skill">Add skill</router-link><div class="separator"></div>
-        <a class="logout" @click="logout">Logout</a>
-      </div>
-    </div>
-    <!-- <router-view :key="$route.fullPath" /> -->
-
+    <Nav :userRole=userRole @logout=logout />
     <router-view v-slot="{ Component }">
       <keep-alive>
         <component :is="Component" :key="$route.fullPath" v-if="$route.meta.keepAlive"></component>
       </keep-alive>
       <component :is="Component" :key="$route.fullPath" v-if="!$route.meta.keepAlive"></component>
     </router-view>
-
   </div>
 </template>
 
 <script>
+
+import Nav from '@/components/Nav.vue'
 
 import "@/assets/global.css";
 import "@/assets/theme_lily.css";
@@ -61,6 +22,7 @@ import "@/assets/theme_haru.css";
 import "@/assets/theme_ephnel.css";
 import "@/assets/theme_chii.css";
 import "@/assets/theme_nabi.css";
+import "@/assets/theme_dana.css";
 import "@/assets/theme_erwin.css";
 import UserService from './services/UserService';
 
@@ -69,6 +31,9 @@ export default {
     return {
       userRole: ''
     }
+  },
+  components: { 
+    Nav
   },
   methods: {
     async logout() {
@@ -147,10 +112,5 @@ export default {
   cursor: pointer;
 }
 
-/* Responsive */
-@media screen and (max-width: 1150px) {
-  #nav {
-    flex-direction: column;
-  }
-}
+
 </style>
