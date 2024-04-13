18009dd23be8f77783742d662c9d72d32ceac9ed
Dai <hoangdai.d93@gmail.com>
2024-04-13 18:50:10 +0200

    feat: add publicPath option

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,9 +1,12 @@
-module.exports = {
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
   productionSourceMap: false,
   configureWebpack: {
     output: {
       filename: '[name]-[contenthash].js', 
       chunkFilename: '[name]_[contenthash].js',
     },
-  }
-};
+  },
+  publicPath: '/'
+})
