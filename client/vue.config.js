d06b683717482c524d54f2f93302e32ef8de80a2
Dai <hoangdai.d93@gmail.com>
2024-04-13 18:11:29 +0200

    feat: add output path webpack

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   productionSourceMap: false,
   configureWebpack: {
     output: {
+      output: '/',
       filename: '[name]-[contenthash].js', 
       chunkFilename: '[name]_[contenthash].js',
     },
